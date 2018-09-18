9176cc3b519434f00cddcda1a400eb103d3d06ae
Daniel O'Connor <Daniel.O'Connor@schneider-electric.com>
2018-09-18 13:58:46 +0930

    Fix per-host lookup. Comment out debug statements

--- OmegaLink.ps1
+++ OmegaLink.ps1
@@ -1,5 +1,5 @@
 # Note: need this to be run first time on a system for logs
-#  New-EventLog –LogName Application –Source "OmegaLink"
+#  New-EventLog -LogName Application -Source "OmegaLink"
 #Write-EventLog -LogName "Application" -Source "OmegaLink" -EventId 1000 -Message "Started"
 
 # Gets the specified registry value or $null if it is missing
@@ -9,26 +9,28 @@ function Get-RegistryValue($path, $name) {
         $key.GetValue($name, $null)
     }
 }
+#Write-EventLog -LogName "Application" -Source "OmegaLink" -EventId 1000 -Message "Called with '$args'"
 
 $uri = [System.Uri]$args[0]
+
 if ($uri.Scheme -ne "omegalink") {
     $tmp = Read-Host -Prompt "Unknown URI scheme"
     Exit 1
 }
 
 # Look for a per-host prefix
-$prefix = Get-RegistryValue Registry::HKEY_CLASSES_ROOT\omegalink "prefix_" + $uri.host
+#Write-EventLog -LogName "Application" -Source "OmegaLink" -EventId 1000 -Message "Host $($uri.host)"
+$prefix = Get-RegistryValue Registry::HKEY_CLASSES_ROOT\omegalink "prefix_$($uri.host)"
 if ($prefix -eq $null) {
     # Otherwise, use the default
     $prefix = Get-RegistryValue Registry::HKEY_CLASSES_ROOT\omegalink prefix
 }
 if ($prefix -eq $null) {
-    $tmp = Read-Host -Prompt "Prefix not set"
+    #$tmp = Read-Host -Prompt "Prefix not set"
     Exit 1
 }
 
-#echo $filename
 $filename = $prefix + $uri.LocalPath -replace "/", "\"
-#Write-EventLog -LogName "Application" -Source "OmegaLink" -EventId 1000 -Message "Opening '$filename'"
+#Write-EventLog -LogName "Application" -Source "OmegaLink" -EventId 1000 -Message "Opening '$filename' prefix '$prefix'"
 $explorer = Join-Path $env:windir explorer.exe
 Start-Process -FilePath $explorer -ArgumentList "/select, ""$filename"""
